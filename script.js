2c8563e7ef6661c513b5d4c92555f32510c82649
gugu256 <gugu256@mail.com>
2023-07-29 18:03:00 +0200

    New system : I can now edit files more easily!

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
+async function readTextFile(filePath) {
+  try {
+      const response = await fetch(filePath);
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+      const fileContent = await response.text();
+      return fileContent; // Return the file content as a resolved value of the promise.
+  } catch (error) {
+      console.error('Error reading the file:', error);
+      throw error; // Rethrow the error to be caught by the caller if needed.
+  }
+}
+
+async function displayFileContent(filePath, targetElementId) {
+  try {
+      const fileContent = await readTextFile(filePath);
+      const targetElement = document.getElementById(targetElementId);
+      targetElement.innerHTML = fileContent;
+  } catch (error) {
+      // Handle any errors that occurred during reading the file.
+  }
+}
+
+// Usage example:
+const filePath = 'https://gugu256.github.io';
+
+
 let projectsToggled = false
 let contactsToggled = false
 let coffeeToggled = false
+let newsToggled = false
 
 function displayProjects() {
   if (projectsToggled == false) {
-    document.getElementById("projectspart").innerHTML = '<h3>Duck Coin (soon to come)</h3><p>Duck Coin (DKC) is the first cryptocurrency that is not based on the blockchain and on GPU mining. Instead, it is based on CPU mining. It is way easier to mine and needs less energy. It\'s still being developed though, so you can\'t download the Duck Coin Client yet (but you\'ll soon to be able to!)</p><h3><a href="https://shitter.ch" target="_blank" rel="noopener noreferrer">Shitter.ch</a></h3><p>Shitter.ch is a free, <a href="https://github.com/gugu256/shitter" target="_blank" rel="noopener noreferrer">open-source</a> and anonymous social media created by my and canard. It was originally created as a joke mocking twitter, but it is now a more <i>serious</i> project.</p><h3>United Qtubers Association</h3><p>U.Q.A. is my musical artist name! By the way, I just realeased a new EP called "Schlö".<a href="https://open.spotify.com/artist/7Hs34IxB6oLLJXhTtccOTv" target="_blank" rel="noopener noreferrer">You can listen to it <u>here.</u></a></p><h3><a href="https://londonian-translator.pancakedev.repl.co" target="_blank" rel="noopener noreferrer">London Accent Translator</a></h3><p>The concept of this website is simple : input normal english, press the button, and you\'ll get a Londonized version of your text!</p><h3><a href="https://organization.thedukk.repl.co" target="_blank" rel="noopener noreferrer">Duck Organization</a></h3><p>The Duck Organization is a series of mini-projects about DUCKS including :<br><u>Worship the Duck</u> : a website where you pretty worship a duck a get higher up in the duck clergy<br><u>Stroke the Duck</u> : a website where you pet/stroke a duck<br><u>Bouncy Duck</u> (formally known as StuDuck) : a website where you can admire a duck bouncing. But you can also start a session of a set number of bounces. When you start a session bounces are reset to 0, Lo-Fi music starts playing in the background and you can start working/studying. At the end of your session, I recommend going on <a href="https://shitter.ch" target="_blank" rel="noopener noreferrer">shitter.ch</a> and writing a post flexing about the length of your working/studying session and the number of times the duck bounced during it<br><u>Feed the Duck</u> : a website your feed a duck and see it grow bigger and bigger</p><h3><a href="https://angugames.itch.io" target="_blank" rel="noopener noreferrer">My itch.io</a></h3><p>I make games sometimes (or at least I used to)</p>'
+    //document.getElementById("projectspart").innerHTML =  readTextFile(filePath);
+    displayFileContent('https://gugu256.github.io/projects.html', 'projectspart');
     document.getElementById("arrowprojects").innerHTML = "▾"
     projectsToggled = true
   } else {
@@ -16,7 +46,7 @@ function displayProjects() {
 
 function displayContacts() {
   if (contactsToggled == false) {
-    document.getElementById("contactspart").innerHTML = '<p>You can talk to me on discord by joining my <u><a href="https://discord.gg/7Xhu2GZw5f" target="_blank" rel="noopener noreferrer">discord server!</a></u><br>If you don\'t have discord, you can still e-mail me : <u><a href="mailto:gugu256@mail.com">gugu256@mail.com</a></u></p>'
+    displayFileContent('https://gugu256.github.io/contacts.html', 'contactspart');
     document.getElementById("arrowcontacts").innerHTML = "▾"
     contactsToggled = true
   } else {
@@ -28,7 +58,7 @@ function displayContacts() {
 
 function displayCoffee() {
   if (coffeeToggled == false) {
-    document.getElementById("coffeepart").innerHTML = '<p>You can donate to me through <a target="_blank" href="https://www.buymeacoffee.com/krayse/"><u>buymeacoffee.com!</u></a></p> <script type="text/javascript" src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js" data-name="bmc-button" data-slug="krayse" data-color="#7c3238" data-emoji=""  data-font="Cookie" data-text="Buy me a coffee" data-outline-color="#ffffff" data-font-color="#ffffff" data-coffee-color="#FFDD00" ></script> <p>You don\'t like this way of donating? No problem! You can give me money through Bitcoin and Ethereum.<br>Here are my wallet addresses : <br>BTC : 3EEDXwPmCv7t39mDBtJ6B5bsRrAyiCXrsz<br>ETH : 0x106Affaf92D82B9778AdB85e8E041768F3B19Fa6</p>'
+    displayFileContent('https://gugu256.github.io/donating.html', 'coffeepart');
     document.getElementById("arrowcoffee").innerHTML = "▾"
     coffeeToggled = true
   } else {
@@ -36,4 +66,17 @@ function displayCoffee() {
     document.getElementById("arrowcoffee").innerHTML = "▸"
     coffeeToggled = false
   }
+}
+
+function displayNews() {
+  if (newsToggled == false) {
+    document.getElementById("newspart").innerHTML = ""
+    document.getElementById("arrownews").innerHTML = "▾"
+    newsToggled = true
+  } else {
+    document.getElementById("newspart").innerHTML = ""
+    document.getElementById("arrownews").innerHTML = "▸"
+    newsToggled = false
+  }
+
 }
